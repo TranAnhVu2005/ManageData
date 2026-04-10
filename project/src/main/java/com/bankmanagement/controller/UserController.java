bb9fd94f2e4c6b7ce45ecfc020e191ea7110643b
TranAnhVu2005 <trananhvu314159@gmail.com>
2026-04-10 11:24:04 +0700

    Fix update info

--- project/src/main/java/com/bankmanagement/controller/UserController.java
+++ project/src/main/java/com/bankmanagement/controller/UserController.java
@@ -218,6 +218,10 @@ public class UserController {
     private void updateInfo() {
         String[] input = view.getUpdateInfoInput();
         String hash = null;
+        if (!UserAccountsDAO.verifyUserPassword(currentUser.getUserId(), input[4])) {
+            System.out.println("Incorrect current password");
+            return;
+        }
         if (!input[3].isEmpty()) {
             hash = org.mindrot.jbcrypt.BCrypt.hashpw(input[3], org.mindrot.jbcrypt.BCrypt.gensalt());
         }
